73d1eb5faa191bc58145075dce70b0886923e60b
mraza <mraza@uw.edu>
2015-12-08 02:40:04 -0800

    Minor

--- src/asml/featgen/hashing.py
+++ src/asml/featgen/hashing.py
@@ -3,6 +3,8 @@ from sklearn.metrics.pairwise import cosine_similarity
 import sys
 import csv
 HASH_LENGTH=2
+num_count_features=13
+num_categorical_features=26
 # length of the hash is going to be logm*8 bits
 def hashing(vector, m=HASH_LENGTH):
 
@@ -14,32 +16,70 @@ def hashing(vector, m=HASH_LENGTH):
 		feature_vector[index]+=sign
 	return feature_vector
 
+def init_agg_dict():
+	dct_agg=defaultdict(float) #dictionary for aggregate measures 
+	#initializing the dct_agg so that every output row has all the columns
+	for clicked in target_fields.keys():
+		dct_agg['Min_'+target_fields[clicked]]=0
+		dct_agg['Count_'+target_fields[clicked]]=0
+		dct_agg['Max_'+target_fields[clicked]]=0
+		dct_agg['Mean_'+target_fields[clicked]]=0
+	return dct_agg
+
+def update_summary_vars(dct_agg,count_val, click):
+
+	dct_agg['Min_'+target_fields[clicked]]=np.amin(dct_agg['Min_'+target_fields[clicked]],count_val)
+	dct_agg['Max_'+target_fields[clicked]]=np.amax(dct_agg['Max_'+target_fields[clicked]],count_val)
+	dct_agg['Mean_'+target_fields[clicked]]=dct_agg['Mean_'+target_fields[clicked]]* dct_agg['Count_'+target_fields[clicked]]
+	dct_agg['Count_'+target_fields[clicked]]+=1
+	return dct_agg
+
+def update_similarity(dct_row, field1, field2):
+
+	for i in range(len(hashable_fields)):
+   		for j in range(i+1, len(hashable_fields)):
+			field1=dct_row[hashable_fields[i]]
+			field2=dct_row[hashable_fields[j]]
+			hash1=hashing(field1)
+			hash2=hashing(field2)
+			similarity=cosine_similarity(hash1, hash2)[0][0]
+	#print 'similarity_'+str(hashable_fields[i])+'_'+str(hashable_fields[j])
+			dct_row['Similarity_'+str(hashable_fields[i])+'_'+str(hashable_fields[j])]=similarity								
+	return dct_row
+	
 def readFile(filename):
-	header=['click','impression','displayURL','AdId','AdvertiserId','Depth','Position','QueryId','KeywordId','TitleId','DescriptionId','UserId','Gender','Age','QueryTokens','DescriptionTokens','KeywordTokens','TitleTokens']
+	count_cols=['Count_'+str(i) for i in range(1,num_count_features+1)]
+	categorical_cols=['Categorical_'+str(i) for i in range(1,num_categorical_features+1)]
+	header=count_cols+categorical_cols
+	target_fields={1:'click',0:'no_click'}
+	header
 	row_dict={}
+	dct_agg=init_agg_dict()
 	with open(filename,'r') as fin:
 		line_count=0
 		csv_r=csv.reader(fin)
+		
 		for row in csv_r:
 			#print row	
 			if line_count==0:
 				print 'Header Read'
 				line_count+=1
 			else:
-				dct={}						
-				for i in range(len(row)):
-					dct[header[i]]=row[i]	
-				hashable_fields=['QueryTokens','DescriptionTokens','KeywordTokens','TitleTokens']
-				for i in range(len(hashable_fields)):
-				   for j in range(i+1, len(hashable_fields)):
-					field1=dct[hashable_fields[i]]
-					field2=dct[hashable_fields[j]]
-					hash1=hashing(field1)
-					hash2=hashing(field2)
-					similarity=cosine_similarity(hash1, hash2)[0][0]
-					#print 'similarity_'+str(hashable_fields[i])+'_'+str(hashable_fields[j])
-					dct['similarity_'+str(hashable_fields[i])+'_'+str(hashable_fields[j])]=similarity								
-				row_dict[line_count-1]=dct# Accounting for header
+				dct_row={}
+
+				# Populating the dictionary so that we can access each item by the field name
+				# First elment is the click / no click
+				for i in range(1,len(row)):
+					dct_row[header[i]]=row[i]	
+				clicked=row[0] # Clicked or not clicked
+				for i in range(len(count_cols)):
+					count_val=dct_row[count_cols[i]]
+					dct_agg=update_summary_vars(dct_agg, count_val, clicked)
+				# Handling categorical features
+				hashable_fields=categorical_cols
+				dct_row=update_smilarity(hashable_fields)
+				dct_row.update(dct_agg)#merging the dictionaries
+				row_dict[line_count-1]=dct_row# Accounting for header
 				line_count+=1
 	
 	fout=open('out.txt','w')
